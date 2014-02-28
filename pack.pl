91b3672b258ef878512515a790248c59e2f50efa
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-02-28 13:42:23 +0100

    Renamed odf-sheet --> odf_sheet

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
-name('odf-sheet').
+name('odf_sheet').
 title('Load and analyze ODF spreadsheets (.ods)').
 version('0.1.0').
 keywords([odf, ods, spreadsheet, table]).
 author('Jan Wielemaker', 'J.Wielemaker@vu.nl').
 requires(webconsole).
-download('http://www.swi-prolog.org/download/pack/odf-sheet-*.tgz').
+download('http://www.swi-prolog.org/download/pack/odf_sheet-*.tgz').
