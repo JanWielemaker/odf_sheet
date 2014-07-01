096c03734056a00288ae807c4ab15f73a336eb25
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-07-01 15:36:15 +0200

    Bumped version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name('odf_sheet').
 title('Load and analyze ODF spreadsheets (.ods)').
-version('0.2.2').
+version('0.2.3').
 keywords([odf, ods, spreadsheet, table]).
 author('Jan Wielemaker', 'J.Wielemaker@vu.nl').
 requires(webconsole).
